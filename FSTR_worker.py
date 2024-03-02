c7c57211fddc4fd66f317a365f4308313b93404a
sevenWatersObserver <pentwistinksplash@inbox.ru>
2024-03-02 20:48:26 +0100

    POST method draft + DB rework
    Created a POST /submitData/ method, then adapted the underlying code and the DB so that it works. Modified DB dump not included yet. Further testing may be needed to check functionality.

--- FSTR_worker.py
+++ FSTR_worker.py
@@ -1,6 +1,11 @@
 import psycopg2
 import json
+from fastapi import FastAPI
 import MOD_DB_LOGIN as S
+
+
+input_expect = {"beauty_title", "title", "other_titles", "connect",
+                "add_time", "user", "coords", "level", "images"}
 
 conn = psycopg2.connect(
     host=S.FSTR_DB_HOST,
@@ -9,29 +14,60 @@ conn = psycopg2.connect(
     password=S.FSTR_DB_PASS,
     port=S.FSTR_DB_PORT
 )
-# cur = conn.cursor()
 
 
-class FSTRDBWorker:
-    # dummy method 1, just returns input data but this does it on the example input
-    # TODO
+class NotEnoughData(Exception):
+    pass
+
+
+class DBdrone:
+    # method that just posts what it reads from "input.json"
     @staticmethod
-    def dummy_input_process():
-        with open("example_input.json", "r", encoding="utf8") as read_f:
-            raw_data = json.load(read_f)
-            return json.dumps(raw_data)
+    def post_raw():
+        read_f = open("input.json", "r", encoding="utf8")
+        processed_input = json.load(read_f)
+        parsed_input = json.dumps(processed_input)
+        read_f.close()
+        if processed_input.keys() < input_expect:
+            raise NotEnoughData
+        else:
+            return parsed_input
+
+
+# REST API goes here
+drone = DBdrone
+app = FastAPI()
+
+
+@app.post("/submitData")
+def post_entry():
+    with conn.cursor() as cur:
+        try:
+            cur.execute("INSERT INTO pereval_added (raw_data) VALUES ('%s') RETURNING id" % drone.post_raw())
+        except psycopg2.OperationalError:
+            return {"status": 500, "message": "Ошибка подключения к базе данных", "id": 'null'}
+        except NotEnoughData:
+            return {"status": 400, "message": "Недостаточно информации", "id": 'null'}
+        else:
+            conn.commit()
+            post_return = cur.fetchall()[0]
+            return {"status": 200, "message": "Запрос успешно отправлен.", "id": '%s' % post_return}
 
 
 # do testing here
 if __name__ == "__main__":
-    drone = FSTRDBWorker()
     # test function, testing cursor
     """with conn.cursor() as cur:
-        cur.execute("SELECT * FROM spr_activities_types"),
-        print(cur.fetchall())
-        print(type(cur.fetchall()))"""
+        cur.execute("SELECT raw_data FROM pereval_added"),
+        print(cur.fetchall())"""
     # test function, updating the first example entry of the database
-    with conn.cursor() as cur:
+    """with conn.cursor() as cur:
         cur.execute("UPDATE pereval_added SET raw_data = '%s' WHERE id = 1" % (drone.dummy_input_process())),
         cur.execute("SELECT raw_data FROM pereval_added WHERE id = 1")
-        print(cur.fetchall())
+        print(cur.fetchall())"""
+    # general testing...
+    # print(post_entry())
+    # with conn.cursor() as cur:
+        # cur.execute("INSERT INTO pereval_added (raw_data) VALUES ('%s')" % drone.post_raw())
+        # cur.execute("SELECT * FROM pereval_added")
+        # print(cur.fetchall())
