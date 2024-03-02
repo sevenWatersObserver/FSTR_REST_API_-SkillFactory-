8a3e3e90895bad87a9c22d7ce5bd53bc3eff4958
sevenWatersObserver <pentwistinksplash@inbox.ru>
2024-03-02 21:31:53 +0100

    Tentative finalizing
    See title.

--- FSTR_worker.py
+++ FSTR_worker.py
@@ -3,7 +3,7 @@ import json
 from fastapi import FastAPI
 import MOD_DB_LOGIN as S
 
-
+# инициализация - массив для проверки наличия данных в input.json, соединение с PostgreSQL БД
 input_expect = {"beauty_title", "title", "other_titles", "connect",
                 "add_time", "user", "coords", "level", "images"}
 
@@ -20,21 +20,24 @@ class NotEnoughData(Exception):
     pass
 
 
+# e
 class DBdrone:
-    # method that just posts what it reads from "input.json"
+    # этот метод просто читает "input.json" и отправляет его
     @staticmethod
     def post_raw():
         read_f = open("input.json", "r", encoding="utf8")
+        # json.load здесь, возможно, магия; неясно что будет если убрать, или как
         processed_input = json.load(read_f)
         parsed_input = json.dumps(processed_input)
         read_f.close()
+        # проверка на наличие данных?
         if processed_input.keys() < input_expect:
             raise NotEnoughData
         else:
             return parsed_input
 
 
-# REST API goes here
+# начало программы, тут стоит FastAPI
 drone = DBdrone
 app = FastAPI()
 
@@ -54,20 +57,11 @@ def post_entry():
             return {"status": 200, "message": "Запрос успешно отправлен.", "id": '%s' % post_return}
 
 
-# do testing here
+# здесь всякие тесты
 if __name__ == "__main__":
-    # test function, testing cursor
-    """with conn.cursor() as cur:
-        cur.execute("SELECT raw_data FROM pereval_added"),
-        print(cur.fetchall())"""
     # test function, updating the first example entry of the database
     """with conn.cursor() as cur:
-        cur.execute("UPDATE pereval_added SET raw_data = '%s' WHERE id = 1" % (drone.dummy_input_process())),
+        cur.execute("UPDATE pereval_added SET raw_data = '%s' WHERE id = 1" % (drone.post_raw())),
         cur.execute("SELECT raw_data FROM pereval_added WHERE id = 1")
         print(cur.fetchall())"""
-    # general testing...
-    # print(post_entry())
-    # with conn.cursor() as cur:
-        # cur.execute("INSERT INTO pereval_added (raw_data) VALUES ('%s')" % drone.post_raw())
-        # cur.execute("SELECT * FROM pereval_added")
-        # print(cur.fetchall())
+    # testing anything else here
